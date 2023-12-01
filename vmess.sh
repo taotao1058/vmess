3903d013c728684d5e8feef19d80046e918c67ec
青春不留白 <115192496+taotao1058@users.noreply.github.com>
2023-12-02 04:26:02 +0800

    Update vmess.sh

--- vmess.sh
+++ vmess.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-author=233boy
-
-# bash fonts colors
+# Define colors
 red='\e[31m'
 yellow='\e[33m'
 green='\e[92m'
 none='\e[0m'
 
-# ...（其他颜色相关函数定义）
-
-is_err=$(_red_bg 错误!)
-is_warn=$(_red_bg 警告!)
+# Error and warning messages
+is_err="${red}错误!${none}"
+is_warn="${red}警告!${none}"
 
 err() {
     echo -e "\n$is_err $@\n" && exit 1
@@ -21,36 +18,36 @@ warn() {
     echo -e "\n$is_warn $@\n"
 }
 
-# root
+# Check if running as root
 [[ $EUID != 0 ]] && err "当前非 ${yellow}ROOT用户.${none}"
 
-# yum or apt-get, ubuntu/debian/centos
+# Check the package manager (yum or apt-get)
 cmd=$(type -P apt-get || type -P yum)
 [[ ! $cmd ]] && err "此脚本仅支持 ${yellow}(Ubuntu or Debian or CentOS)${none}."
 
-# systemd
+# Check for systemd
 [[ ! $(type -P systemctl) ]] && {
     err "此系统缺少 ${yellow}(systemctl)${none}, 请尝试执行:${yellow} ${cmd} update -y;${cmd} install systemd -y ${none}来修复此错误."
 }
 
-# wget installed or none
+# Check if wget is installed
 is_wget=$(type -P wget)
+[[ ! $is_wget ]] && err "请安装 wget."
 
-# x64
+# Architecture check (x64 or arm64)
 case $(uname -m) in
-amd64 | x86_64)
-    is_jq_arch=amd64
-    is_core_arch="64"
-    ;;
-*aarch64* | *armv8*)
-    is_jq_arch=arm64
-    is_core_arch="arm64-v8a"
-    ;;
-*)
-    err "此脚本仅支持 64 位系统..."
-    ;;
+    amd64 | x86_64)
+        is_core_arch="64"
+        ;;
+    *aarch64* | *armv8*)
+        is_core_arch="arm64-v8a"
+        ;;
+    *)
+        err "此脚本仅支持 64 位系统..."
+        ;;
 esac
 
+# Variables
 is_core=v2ray
 is_core_name=V2Ray
 is_core_dir=/etc/$is_core
@@ -60,73 +57,37 @@ is_conf_dir=$is_core_dir/conf
 is_log_dir=/var/log/$is_core
 is_sh_bin=/usr/local/bin/$is_core
 is_sh_dir=$is_core_dir/sh
-is_sh_repo=$author/$is_core
+is_sh_repo=233boy/$is_core
 is_pkg="wget unzip"
 is_config_json=$is_core_dir/config.json
-tmp_var_lists=(
-    tmpcore
-    tmpsh
-    tmpjq
-    is_core_ok
-    is_sh_ok
-    is_jq_ok
-    is_pkg_ok
-)
 
-# ...（其他函数定义）
-
-# tmp dir
+# Temporary directory
 tmpdir=$(mktemp -u)
-[[ ! $tmpdir ]] && {
-    tmpdir=/tmp/tmp-$RANDOM
-}
+[[ ! $tmpdir ]] && tmpdir=/tmp/tmp-$RANDOM
 
-# set up var
-for i in ${tmp_var_lists[*]}; do
-    export $i=$tmpdir/$i
-done
-
-# load bash script.
-load() {
-    . $is_sh_dir/src/$1
-}
-
-# wget add --no-check-certificate
+# Function to download file using wget
 _wget() {
     [[ $proxy ]] && export https_proxy=$proxy
     wget --no-check-certificate $*
 }
 
-# print a mesage
+# Function to print messages
 msg() {
     case $1 in
-    warn)
-        local color=$yellow
-        ;;
-    err)
-        local color=$red
-        ;;
-    ok)
-        local color=$green
-        ;;
+        warn)
+            local color=$yellow
+            ;;
+        err)
+            local color=$red
+            ;;
+        ok)
+            local color=$green
+            ;;
     esac
-
     echo -e "${color}$(date +'%T')${none}) ${2}"
 }
 
-# show help msg
-show_help() {
-    echo -e "Usage: $0 [-f xxx | -l | -p xxx | -v xxx | -h]"
-    echo -e "  -f, --core-file <path>          自定义 $is_core_name 文件路径, e.g., -f /root/${is_core}-linux-64.zip"
-    echo -e "  -l, --local-install             本地获取安装脚本, 使用当前目录"
-    echo -e "  -p, --proxy <addr>              使用代理下载, e.g., -p http://127.0.0.1:2333 or -p socks5://127.0.0.1:2333"
-    echo -e "  -v, --core-version <ver>        自定义 $is_core_name 版本, e.g., -v v5.4.1"
-    echo -e "  -h, --help                      显示此帮助界面\n"
-
-    exit 0
-}
-
-# install dependent pkg
+# Install dependent packages
 install_pkg() {
     cmd_not_found=
     for i in $*; do
@@ -149,37 +110,83 @@ install_pkg() {
     fi
 }
 
-# download file
-download() {
-    case $1 in
-    core)
-        link=https://github.com/${is_core_repo}/releases/latest/download/${is_core}-linux-${is_core_arch}.zip
-        [[ $is_core_ver ]] && link="https://github.com/${is_core_repo}/releases/download/${is_core_ver}/${is_core}-linux-${is_core_arch}.zip"
-        name=$is_core_name
-        tmpfile=$tmpcore
-        is_ok=$is_core_ok
-        ;;
-    sh)
-        link=https://github.com/${is_sh_repo}/releases/latest/download/code.zip
-        name="$is_core_name 脚本"
-        tmpfile=$tmpsh
-        is_ok=$is_sh_ok
-        ;;
-    jq)
-        link=https://github.com/jqlang/jq/releases/download/jq-1.7rc1/jq-linux-$is_jq_arch
-        name="jq"
-        tmpfile=$tmpjq
-        is_ok=$is_jq_ok
-        ;;
-    esac
-
-    msg warn "下载 ${name} > ${link}"
-    if _wget -t 3 -q -c $link -O $tmpfile; then
-        mv -f $tmpfile $is_ok
+# Download V2Ray core file
+download_core() {
+    link=https://github.com/${is_core_repo}/releases/latest/download/${is_core}-linux-${is_core_arch}.zip
+    [[ $is_core_ver ]] && link="https://github.com/${is_core_repo}/releases/download/${is_core_ver}/${is_core}-linux-${is_core_arch}.zip"
+    msg warn "下载 ${is_core_name} > ${link}"
+    if _wget -t 3 -q -c $link -O $tmpdir/v2ray.zip; then
+        unzip -qo $tmpdir/v2ray.zip -d $is_core_dir/bin
+    else
+        msg err "下载 ${is_core_name} 失败"
+        exit 1
     fi
 }
 
-# get server ip
-get_ip() {
-    export "$(_wget -4 -qO- https://one.one.one.one/cdn-cgi/trace | grep ip=)" &>/dev/null
-    [[ -z $ip ]] && export "$(_wget -6 -qO- https://one.one.one.one/cdn-cgi/trace | grep ip=)" &>/dev/null
+# Main function
+main() {
+    # Check if V2Ray is already installed
+    [[ -f $is_core_bin && -d $is_core_dir/bin && -d $is_sh_dir && -d $is_conf_dir ]] && {
+        err "检测到 ${is_core_name} 已安装."
+    }
+
+    # Check for root privilege
+    [[ $EUID != 0 ]] && err "当前非 ${yellow}ROOT用户.${none}"
+
+    # Check for systemd
+    [[ ! $(type -P systemctl) ]] && {
+        err "此系统缺少 ${yellow}(systemctl)${none}, 请尝试执行:${yellow} ${cmd} update -y;${cmd} install systemd -y ${none}来修复此错误."
+    }
+
+    # Check if wget is installed
+    is_wget=$(type -P wget)
+    [[ ! $is_wget ]] && err "请安装 wget."
+
+    # Architecture check (x64 or arm64)
+    case $(uname -m) in
+        amd64 | x86_64)
+            is_core_arch="64"
+            ;;
+        *aarch64* | *armv8*)
+            is_core_arch="arm64-v8a"
+            ;;
+        *)
+            err "此脚本仅支持 64 位系统..."
+            ;;
+    esac
+
+    # Create temporary directory
+    mkdir -p $tmpdir
+
+    # Install dependent packages
+    install_pkg $is_pkg
+
+    # Download V2Ray core
+    download_core
+
+    # Create core command
+    ln -sf $is_core_dir/sh/$is_core.sh $is_sh_bin
+
+    # Create log directory
+    mkdir -p $is_log_dir
+
+    # Print a success message
+    msg ok "生成配置文件..."
+
+    # Create systemd service
+    load systemd.sh
+    is_new_install=1
+    install_service $is_core &>/dev/null
+
+    # Create configuration directory
+    mkdir -p $is_conf_dir
+
+    # Load core script
+    load core.sh
+
+    # Print a success message
+    msg ok "安装完成!"
+}
+
+# Start
+main
