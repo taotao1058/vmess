9cea92f367db910d62cb5d6d51dfaf38fe085b67
青春不留白 <115192496+taotao1058@users.noreply.github.com>
2023-12-02 05:06:33 +0800

    Update vmess.sh

--- vmess.sh
+++ vmess.sh
@@ -6,24 +6,14 @@ yellow='\e[33m'
 green='\e[92m'
 none='\e[0m'
 
-# Error and warning messages
-is_err="${red}错误!${none}"
-is_warn="${red}警告!${none}"
-
-err() {
-    echo -e "\n$is_err $@\n" && exit 1
-}
-
-warn() {
-    echo -e "\n$is_warn $@\n"
-}
-
 # Check if running as root
-[[ $EUID != 0 ]] && err "当前非 ${yellow}ROOT用户.${none}"
+[[ $EUID != 0 ]] && {
+    echo -e "\n${red}错误!${none} 当前非 ${yellow}ROOT用户.${none}\n" && exit 1
+}
 
 # Check if xray is already installed
 if type xray &>/dev/null; then
-    warn "检测到 xray 已安装，继续执行后续步骤."
+    echo "检测到 xray 已安装，继续执行后续步骤."
 else
     # Install xray
     echo "正在安装 xray..."
@@ -31,7 +21,7 @@ else
 
     # Check if xray installation is successful
     if ! type xray &>/dev/null; then
-        err "xray 安装失败，请检查安装脚本是否正常运行."
+        echo -e "\n${red}错误!${none} xray 安装失败，请检查安装脚本是否正常运行.\n" && exit 1
     fi
 fi
 
@@ -50,7 +40,7 @@ local_ip=$(curl -s http://api64.ipify.org)
 
 # Check if local_ip is a valid IPv4 address
 if [[ ! $local_ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    warn "无法获取本机IPv4地址，请手动输入."
+    echo "无法获取本机IPv4地址，请手动输入."
     read -p "请输入服务器的IPv4地址: " local_ip
 fi
 
@@ -105,4 +95,4 @@ systemctl restart xray
 
 # Display one-click connection information
 echo -e "\n${green}一键连接信息:${none}"
-echo -e "${green}vmess://${vmess_id}@${local_ip}:${vmess_port}?encryption=none${none}\n"
+echo -e "${green}vmess://${vmess_id}@${local_ip}:${vmess_port}?security=none&alterId=64&encryption=none${none}\n"
